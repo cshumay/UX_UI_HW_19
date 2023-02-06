a5acc9137a5aaa2b62bb565e99deaaf41f730bb2
cshumay <111400248+cshumay@users.noreply.github.com>
2023-02-06 16:57:51 -0500

    Added music through for a user interaction

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+function playMyAudio(){
+  document.getElementById("myAudio").play();
+ document.getElementById("audioStatus").innerHTML="Audio is Playing";	 
+ 
+}
+function pauseMyAudio(){
+  document.getElementById("myAudio").pause();
+ document.getElementById("audioStatus").innerHTML="Audio Paused";	
+}
+
 function fade() {
   $('.preloader').fadeOut("slow");
   }
